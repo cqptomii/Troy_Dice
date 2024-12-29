a51a5dd76fd963d0073fa47fa5e698bded9d6687
Tom <tomfraisse.pro@gmail.com>
2024-12-29 17:19:25 +0100

    Ajout images

---
+++ src/module-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Troy_dice{
+	requires transitive javafx.controls;
+	
+	requires transitive javafx.graphics;
+	requires transitive javafx.base;
+	requires transitive javafx.fxml;
+	
+	exports gui;
+}
+
